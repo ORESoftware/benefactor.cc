864a83b1471f7719b5d0b559fb2ebd9dca3fd22a
Alexander Mills <alexander.d.mills@gmail.com>
2026-02-24 13:08:09 -0600

    init

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://benefactor.cc',
+  server: {
+    port: 4323,
+  },
+});
