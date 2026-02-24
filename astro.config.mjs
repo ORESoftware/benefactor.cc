1381c90334dc1286fda7cd99be98efb96c6aca7e
Alexander Mills <alexander.d.mills@gmail.com>
2026-02-24 13:18:28 -0600

    Add GitHub Actions deploy workflow + set base path for GH Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://benefactor.cc',
+  site: 'https://oresoftware.github.io',
+  base: '/benefactor.cc',
   server: {
     port: 4323,
   },
